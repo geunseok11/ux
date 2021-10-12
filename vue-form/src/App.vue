037f254d0e822aff9fd40b283963d32060c2025a
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-10-12 16:34:34 +0900

    FIX App.vue

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="username">ID:</label>
+        <input
+          id="username"
+          type="text"
+          v-model="username"
+          class="username-input"
+          v-bind:class="{ 'error': isError}"
+        />
+      </div>
+      <div>
+        <label for="password">PW:</label>
+        <input id="password" type="password" v-model="password" />
+      </div>
+      <button v-bind:disabled="!isUsernameValid" type="submit">LOGIN</button>
+    </form>
+    <p v-if="isError">옳바르지 않은 ID 입니다</p>
+    <p v-if="isUsernameValid">이메일 형식이 맞습니다</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      username: "",
+      password: "",
+      isError: false
+    };
+  },
+  computed: {
+    isUsernameValid() {
+      return validateEmail(this.username);
+    }
+  },
+  methods: {
+    submitForm() {
+      console.log("log in");
+      // axios.post().then().catch(err => { this.isError = true})
+      this.isError = true;
+      // this.initForm();
+    },
+    initForm() {
+      this.username = "";
+      this.password = "";
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.username-input {
+  outline: none;
+}
+.username-input.error {
+  border: 1px solid red;
 }
 </style>
