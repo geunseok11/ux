e9625d7abfa29a6be322e0d7b036dcdd1944aa7d
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-10-13 22:31:52 +0900

    ADD progressBar

---
+++ vue-form/src/components/ProgressBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="progress"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.progress {
+  background-color: lime;
+  height: 5px;
+  width: 0%;
+  /* transition: width 700ms linear; */
+  animation: loading 1s linear;
+}
+.complete {
+  width: 100%;
+}
+@keyframes loading {
+  0% {
+    width: 0%;
+  }
+  50% {
+    width: 50%;
+  }
+  100% {
+    width: 100%;
+  }
+}
+</style>
